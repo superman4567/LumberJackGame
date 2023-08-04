f7bfea4bc89f1ea264312ce637bb566288d1710f
Nathan Steendam <bo2nathan@hotmail.com>
2023-08-05 00:59:39 +0200

    [ADD] Health and Stamina setup

--- Assets/Scripts/RadialProgressBar.cs
+++ Assets/Scripts/RadialProgressBar.cs
@@ -35,7 +35,6 @@ public class RadialProgressBar : MonoBehaviour
     {
         ShowRadialUI();
         ResetRadialUI();
-        Debug.Log(isInteracting);
     }
 
     private void InteractingTrue()
