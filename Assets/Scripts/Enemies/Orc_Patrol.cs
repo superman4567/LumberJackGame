5ca7e1d39faae3353375ba4b93edcbe88c1470ac
Nathan Steendam <bo2nathan@hotmail.com>
2023-08-11 22:59:24 +0200

    [FIX] Rock throw bugg and better chasing/alerting

--- Assets/Scripts/Enemies/Orc_Patrol.cs
+++ Assets/Scripts/Enemies/Orc_Patrol.cs
@@ -5,43 +5,75 @@ using UnityEngine.AI;
 
 public class Orc_Patrol : MonoBehaviour
 {
-    public Transform[] patrolPoints; // Define the patrol points in the Inspector
-    public string playerTag = "Player"; // Tag of the player GameObject
-    public float playerDetectionRadius = 5.0f; // Distance at which the AI detects the player
-    public float alarmRadius = 10.0f; // Distance at which the AI alerts nearby AI
-    private NavMeshAgent navMeshAgent;
+    [SerializeField] private Transform[] patrolPoints; 
+    [SerializeField] private float playerDetectionRadius = 5.0f; 
+    [SerializeField] private float alarmRadius = 10.0f; 
+    [SerializeField] private float chaseDuration = 10.0f;
+    [SerializeField] private NavMeshAgent navMeshAgent;
+    [SerializeField] private Transform player;
 
+    public bool isAlerted = false;
+    public bool isChasing = false; 
+    private float chaseTimer = 0.0f;
     private int currentPatrolIndex = 0;
-    private Transform player;
-
+    
     private void Start()
     {
-        navMeshAgent = GetComponent<NavMeshAgent>();
-        player = GameObject.FindGameObjectWithTag(playerTag)?.transform;
-
-        if (player == null)
-        {
-            Debug.LogWarning("Player with tag '" + playerTag + "' not found in the scene.");
-        }
-
+        ShufflePatrolPoints();
         SetNextPatrolPoint();
+        navMeshAgent.speed = 2f;
     }
 
     private void Update()
     {
         if (player != null)
         {
-            if (IsPlayerNearby())
+            if (IsPlayerNearby() || isAlerted)
             {
-                navMeshAgent.destination = player.position;
-
-                // Alert nearby AI if the player is detected
+                isChasing = true;
+                navMeshAgent.speed = 5f;
+                chaseTimer = 0.0f;
+                CheckIfChasing();
                 AlertNearbyAI();
             }
-            else if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance < 0.5f)
+            else if (!IsPlayerNearby())
             {
-                SetNextPatrolPoint();
+                isChasing = false;
+                navMeshAgent.speed = 2f;
             }
+        }
+    }
+
+    public void CheckIfChasing()
+    {
+        if (isChasing && chaseTimer >= chaseDuration)
+        {
+            isChasing = false;
+            isAlerted = false;
+            SetNextPatrolPoint();
+        }
+        else
+        {
+            chaseTimer += Time.deltaTime;
+            navMeshAgent.destination = player.position;
+        }
+
+        if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance < 0.5f)
+        {
+            SetNextPatrolPoint();
+        }
+    }
+    
+
+    private void ShufflePatrolPoints()
+    {
+        // Fisher-Yates shuffle algorithm
+        for (int i = patrolPoints.Length - 1; i > 0; i--)
+        {
+            int j = Random.Range(0, i + 1);
+            Transform temp = patrolPoints[i];
+            patrolPoints[i] = patrolPoints[j];
+            patrolPoints[j] = temp;
         }
     }
 
@@ -54,12 +86,13 @@ public class Orc_Patrol : MonoBehaviour
         currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
     }
 
-    private bool IsPlayerNearby()
+    public bool IsPlayerNearby()
     {
         if (player == null)
             return false;
 
         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        //This returns true
         return distanceToPlayer <= playerDetectionRadius;
     }
 
@@ -78,6 +111,15 @@ public class Orc_Patrol : MonoBehaviour
         }
     }
 
+    public void OnAlert(Vector3 alertPosition)
+    {
+        if (!isAlerted)
+        {
+            isAlerted = true;
+            navMeshAgent.destination = alertPosition;
+        }
+    }
+
     private void OnDrawGizmosSelected()
     {
         // Draw the player detection radius
@@ -87,12 +129,5 @@ public class Orc_Patrol : MonoBehaviour
         // Draw the alarm radius
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, alarmRadius);
-    }
-
-    public void OnAlert(Vector3 alertPosition)
-    {
-        // You can implement the behavior for the alerted AI here
-        // For example, set the destination to the alert position
-        navMeshAgent.destination = alertPosition;
     }
 }
